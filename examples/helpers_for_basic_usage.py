f4ea8a3da444830369fe12aa309c839e39101f54
Matthew Fitzpatrick <mrfitzpa@uvic.ca>
2025-10-19 23:19:35 -0700

    Forgot to add boilerplate copyright statements to a file.

--- examples/helpers_for_basic_usage.py
+++ examples/helpers_for_basic_usage.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2024 Matthew Fitzpatrick.
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <https://www.gnu.org/licenses/gpl-3.0.html>.
 """This module contains helper functions for the Jupyter notebook located at
 ``<root>/examples/basic_usage.ipynb``, where ``<root>`` is the root of the
 ``empix`` repository.
